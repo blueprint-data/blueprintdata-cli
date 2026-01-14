d63c5db0f463ba65d3a8152392556207c0637173
juanpiRiv <juanpirivero015@gmail.com>
2026-01-14 13:57:38 -0300

    Feat:Add new postgres lite template

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -4,8 +4,8 @@ import { handleNewProject } from '../prompts/index.js';
 export const newCommand = new Command('new')
   .description('Create a new BlueprintData project')
   .argument('[project-name]', 'Name of the project')
-  .option('--stack <type>', 'Stack type (lite)', 'lite')
-  .option('--storage <type>', 'Storage type (postgres, bigquery)', 'postgres')
+  .option('--stack <type>', 'Stack type (lite, lite-postgres, aws)')
+  .option('--storage <type>', 'Storage type (postgres, bigquery)')
   .action(async (projectName: string | undefined, options) => {
     try {
       await handleNewProject({ projectName, ...options });
